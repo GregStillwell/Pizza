50206f39786888476b7cc3b9346c2fecf6ae33b9
Greg <Jariye05@gmail.com>
2023-07-16 00:03:04 -0700

    Finish first test and added README

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+}
+
+function pizza(pizzaToppings, pizzaSize)  {
+this.toppings = pizzaToppings;
+this.size = pizzaSize
+};
+
+const myPizza = new pizza(["Pepperoni","Sausage"],"medium");
+
+
