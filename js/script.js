45412b312086942eac52c0655b097e3c8ad55c73
Greg <Jariye05@gmail.com>
2023-07-17 21:05:08 -0700

    CR interview and tutor

--- js/script.js
+++ js/script.js
@@ -1,21 +1,58 @@
 //business logic
-window.onload = function() {
+function pizza(pizzaToppings, pizzaSize) {
+  this.toppings = pizzaToppings;
+  this.size = pizzaSize;
+  this.price = 0
+}
 
+pizza.prototype.getPrice()	{
+  // based off of the the amount of toppings
+  // added to the size price
+  // this.price
+}
+
+const myPizza = new pizza(['Pepperoni', 'Sausage'], 'medium');
+
+
+
+//ui logic
+window.onload = function () {
+  const form = document.getElementById("pizzatoppings")
+	form.addEventListener("submit", handleFormSubmit) 
+};
+
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+
+  let selectedToppings = [];
+  // look at form see which checkboxes are checked
+  const toppings = document.getElementsByName("toppings") //array of the HTML elements that have the name toppping
+
+  // put those into a new array
+  //toppings.forEach(function ())
+  
+  for (let i = 0; i < toppings.length; i++){
+    if (toppings[i].checked === true) {
+      selectedToppings.push(toppings[i].value);
+    }
+  }
+
+  toppings.forEach((topping) => {
+    if (topping.checked === true) {
+      selectedToppings.push(topping.value);
+    }
+  })
+
+  
+  const userPizza = new pizza(selectedToppings, 'large');
+  userPizza.getPrice();
+
+  const pizzaOutput = document.getElementById('pizzaOutput');
+
+  pizzaOutput.innerText(`Your Pizza wil cost ${userPizza.price}. And has ${userPizza.toppings} for toppings`)
 
 }
 
-function pizza(pizzaToppings, pizzaSize)  {
-this.toppings = pizzaToppings;
-this.size = pizzaSize
-};
-
-const myPizza = new pizza(["Pepperoni","Sausage"],"medium");
-
-
-
-
-
-
-
-//ul logic
 
